e4ad2b3d7f9b81b2b36374c5783fdcdd089b4185
kevinneyer <kevinneyer@gmail.com>
2020-11-17 16:12:52 -0500

    solved lab

--- index.js
+++ index.js
@@ -1,3 +1,26 @@
 function findMinAndRemoveSorted(array){
-    return array.splice(0, 1)
+    return parseInt(array.splice(0, 1))
+}
+
+function merge(firstHalf, secondHalf){
+    let sortedArray = []
+    let minimum;
+    while(firstHalf.length > 0 && secondHalf.length > 0){
+        if(firstHalf[0] < secondHalf[0]){
+            minimum = findMinAndRemoveSorted(firstHalf)
+            sortedArray.push(minimum)
+        } else if
+        (secondHalf[0] < firstHalf[0]){
+            minimum = findMinAndRemoveSorted(secondHalf)
+            sortedArray.push(minimum)
+        }
+    }
+  return sortedArray.concat(firstHalf).concat(secondHalf)
+}
+
+function mergeSort(array){
+   let median = Math.floor((array.length)/ 2)
+   let firstHalf = array.slice(0, median).sort((a,b) => a < b ? - 1 : 1)
+   let secondHalf = array.slice(median).sort((a,b) => a < b ? - 1 : 1)
+   return merge(firstHalf, secondHalf)
 }
